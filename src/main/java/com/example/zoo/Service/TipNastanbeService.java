16bc7c278c767abb8e30310adec2a750a95ea49c
franpavlovi <fljubuski@gmail.com>
2024-06-01 20:05:22 +0200

    update,delete TipNastambe

--- src/main/java/com/example/zoo/Service/TipNastanbeService.java
+++ src/main/java/com/example/zoo/Service/TipNastanbeService.java
@@ -1,7 +1,9 @@
 package com.example.zoo.Service;
 
+import com.example.zoo.models.OblikNastanbe;
 import com.example.zoo.models.TipNastanbe;
 import com.example.zoo.repository.TipNastanbeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,18 @@ public class TipNastanbeService {
 
     public List<TipNastanbe> findAll(){
         return tipNastanbeRepository.findAll();
+    }
+
+    public void deleteTipNastanbe(Long id){ tipNastanbeRepository.deleteById(id);}
+
+    public TipNastanbe updateTipNastanbe(Long id , TipNastanbe noviTip){
+        TipNastanbe tipNastanbe = tipNastanbeRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Tip nastanbe sa ID-om" + id + "nije pronađen"));
+
+        tipNastanbe.setNaziv(noviTip.getNaziv());
+
+        return tipNastanbeRepository.save(tipNastanbe);
+
     }
 
 }
