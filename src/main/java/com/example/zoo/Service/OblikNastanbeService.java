69b36c890b6c35fb9c694bf0745a60439875a536
franpavlovi <fljubuski@gmail.com>
2024-06-01 19:58:55 +0200

    update,delete OblikNastambe

--- src/main/java/com/example/zoo/Service/OblikNastanbeService.java
+++ src/main/java/com/example/zoo/Service/OblikNastanbeService.java
@@ -2,6 +2,7 @@ package com.example.zoo.Service;
 
 import com.example.zoo.models.OblikNastanbe;
 import com.example.zoo.repository.OblikNastanbeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +21,17 @@ public class OblikNastanbeService {
     public List<OblikNastanbe> findAll(){
         return oblikNastanbeRepository.findAll();
     }
+
+    public void deleteOblikNastanbe(Long id){ oblikNastanbeRepository.deleteById(id);}
+
+    public OblikNastanbe updateOblikNastanbe(Long id , OblikNastanbe noviOblik){
+        OblikNastanbe oblikNastanbe = oblikNastanbeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Oblik nastanbe sa ID-om" + id + "nije pronađen"));
+
+        oblikNastanbe.setNaziv(noviOblik.getNaziv());
+
+        return oblikNastanbeRepository.save(oblikNastanbe);
+
+    }
+
 }
