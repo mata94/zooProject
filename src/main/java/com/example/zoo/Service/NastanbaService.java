995ccf9d0fabebf1549cd6cc15d2a06e21b70e99
franpavlovi <fljubuski@gmail.com>
2024-06-01 20:13:07 +0200

    update,delete Nastamba

--- src/main/java/com/example/zoo/Service/NastanbaService.java
+++ src/main/java/com/example/zoo/Service/NastanbaService.java
@@ -2,6 +2,7 @@ package com.example.zoo.Service;
 
 import com.example.zoo.models.Nastanba;
 import com.example.zoo.repository.NastanbaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,23 @@ public class NastanbaService {
 
     public List<Nastanba> findAll(){
         return this.nastanbaRepository.findAll();
+    }
+
+    public void deleteNastanba(Long id){ nastanbaRepository.deleteById(id);}
+
+    public Nastanba updateNastanba(Long id, Nastanba novaNastanba){
+        Nastanba nastanba = nastanbaRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Nastamba sa ID-om " + id + "nije pronađena"));
+
+        nastanba.setBrojcanaLokacija(novaNastanba.getBrojcanaLokacija());
+        nastanba.setNaziv(novaNastanba.getNaziv());
+        nastanba.setMjestoPromatranja(novaNastanba.getMjestoPromatranja());
+        nastanba.setOblikNastanbe(novaNastanba.getOblikNastanbe());
+        nastanba.setTipNastanbe(novaNastanba.getTipNastanbe());
+        nastanba.setStrukturaNastanbe(novaNastanba.getStrukturaNastanbe());
+
+        return nastanbaRepository.save(nastanba);
+
     }
 
 
