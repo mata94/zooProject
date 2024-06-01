e52d65adb0e9dfbf5a85a727f1c5bde83b97808a
franpavlovi <fljubuski@gmail.com>
2024-06-02 01:32:12 +0200

    ListaOblikNastambi template

--- src/main/java/com/example/zoo/Controllers/OblikNastanbeController.java
+++ src/main/java/com/example/zoo/Controllers/OblikNastanbeController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class OblikNastanbeController {
@@ -28,5 +31,18 @@ public class OblikNastanbeController {
         this.oblikNastanbeService.createOblikNastanbe(oblikNastanbe);
 
         return "redirect:/oblikNastanbe";
+    }
+
+    @GetMapping("/listOblikNastanbe")
+    public String getAllOblikNastanbe(Model model){
+        List<OblikNastanbe> oblikNastanbe = oblikNastanbeService.findAll();
+        model.addAttribute("oblikNastanbe", oblikNastanbe);
+        return "OblikNastanbe/ListOblikNastanbe";
+    }
+
+    @GetMapping("/listOblikNastanbe/delete/{id}")
+    public String deleteOblikNastanbe(@PathVariable Long id){
+        oblikNastanbeService.deleteOblikNastanbe(id);
+        return "redirect:/listOblikNastanbe";
     }
 }
