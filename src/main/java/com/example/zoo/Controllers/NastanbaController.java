de49408f70d5a8c26c8e6d30cbea493c218dc063
franpavlovi <fljubuski@gmail.com>
2024-06-01 21:13:45 +0200

    EditNastamba template

--- src/main/java/com/example/zoo/Controllers/NastanbaController.java
+++ src/main/java/com/example/zoo/Controllers/NastanbaController.java
@@ -1,17 +1,19 @@
 package com.example.zoo.Controllers;
 
-
 import com.example.zoo.Service.NastanbaService;
 import com.example.zoo.Service.OblikNastanbeService;
 import com.example.zoo.Service.TipNastanbeService;
 import com.example.zoo.models.Nastanba;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class NastanbaController {
@@ -47,6 +49,31 @@ public class NastanbaController {
         List<Nastanba> nastambe = nastanbaService.findAll();
         model.addAttribute("nastambe", nastambe);
         return "Nastanba/ListNastanba";
+    }
+
+    @GetMapping("/listNastanba/delete/{id}")
+    public String deleteNastanba(@PathVariable Long id) {
+        nastanbaService.deleteNastanba(id);
+        return "redirect:/listNastanba";
+    }
+
+    @GetMapping("/listNastanba/edit/{id}")
+    public String editNastanbaForm(@PathVariable Long id, Model model) {
+        Optional<Nastanba> optionalNastanba = nastanbaService.findById(id);
+        if (optionalNastanba.isEmpty()) {
+            throw new EntityNotFoundException("Nastanba sa ID-om " + id + " nije pronađena");
+        }
+        Nastanba nastanba = optionalNastanba.get();
+        model.addAttribute("nastanba", nastanba);
+        model.addAttribute("tipNastanbeList", tipNastanbeService.findAll());
+        model.addAttribute("oblikNastanbeList", oblikNastanbeService.findAll());
+        return "Nastanba/EditNastanba";
+    }
+
+    @PostMapping("/listNastanba/edit/{id}")
+    public String updateNastanba(@PathVariable Long id, Nastanba nastanba) {
+        nastanbaService.updateNastanba(id, nastanba);
+        return "redirect:/listNastanba";
     }
 
 }
--- src/main/java/com/example/zoo/Service/NastanbaService.java
+++ src/main/java/com/example/zoo/Service/NastanbaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NastanbaService {
@@ -37,6 +38,8 @@ public class NastanbaService {
         return nastanbaRepository.save(nastanba);
 
     }
+
+    public Optional<Nastanba> findById(Long id) { return nastanbaRepository.findById(id);}
 
 
 }
