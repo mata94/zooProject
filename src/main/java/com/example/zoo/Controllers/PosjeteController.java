0af3794ae652be45a0a7611a3f75ebd241469625
franpavlovi <fljubuski@gmail.com>
2024-06-02 12:02:55 +0200

    ListaPosjeta template

--- src/main/java/com/example/zoo/Controllers/PosjeteController.java
+++ src/main/java/com/example/zoo/Controllers/PosjeteController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -36,5 +37,20 @@ public class PosjeteController {
         posjeteService.save(posjete);
         return "redirect:/unosPosjete";
     }
+
+    @GetMapping("/listPosjeta")
+    public String getAllPosjeta(Model model){
+        List<Posjete> posjete = posjeteService.findAll();
+        model.addAttribute("posjete", posjete);
+        return "Posjeta/ListPosjeta";
+    }
+
+    @GetMapping
+    public String deletePosjeta(@PathVariable Long id){
+        posjeteService.deletePosjeta(id);
+        return "redirect:/listPosjeta";
+    }
+
+
 }
 
--- src/main/java/com/example/zoo/Service/PosjeteService.java
+++ src/main/java/com/example/zoo/Service/PosjeteService.java
@@ -1,9 +1,13 @@
 package com.example.zoo.Service;
 
+import com.example.zoo.models.Nastanba;
 import com.example.zoo.models.Posjete;
 import com.example.zoo.repository.PosjetaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PosjeteService {
@@ -14,5 +18,10 @@ public class PosjeteService {
     public void save(Posjete posjete) {
         posjeteRepository.save(posjete);
     }
+
+    public List<Posjete> findAll() { return this.posjeteRepository.findAll(); }
+
+    public void deletePosjeta(Long id) { posjeteRepository.deleteById(id);}
+
 }
 
