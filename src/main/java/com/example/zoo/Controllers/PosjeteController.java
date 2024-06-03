a162a67d084e85f7e650c602d8e9e47b126b0f19
josippreva <josip.previsic@fsre.sum.ba>
2024-06-03 20:43:45 +0200

    commit message

--- src/main/java/com/example/zoo/Controllers/PosjeteController.java
+++ src/main/java/com/example/zoo/Controllers/PosjeteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class PosjeteController {
+public class gitPosjeteController {
 
     @Autowired
     private PosjeteService posjeteService;
