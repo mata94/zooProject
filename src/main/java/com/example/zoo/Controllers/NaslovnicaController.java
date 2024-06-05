0444703968ee25b91009d218607f22d750d3c964
franpavlovi <fljubuski@gmail.com>
2024-06-05 20:01:24 +0200

    PocetnaStranica template & controller

---
+++ src/main/java/com/example/zoo/Controllers/NaslovnicaController.java
@@ -0,0 +1,16 @@
+package com.example.zoo.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NaslovnicaController {
+
+    @GetMapping("/pocetna")
+    public String pocetna(){
+
+        return "Naslovnica/PocetnaStranica";
+
+    }
+}
